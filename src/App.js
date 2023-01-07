5bd5955dd4c9bf65ef0e800473dc315bf983cdeb
Antonio Thiago <antoniobackenddeveloper@gmail.com>
2023-01-07 19:46:26 -0300

    initial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Template Basico para estudo Antonio Thiago &copy;</h1>
+      <h2>
+        Este template foi feito apenas para fins didaticos para entender como
+        funciona a montagem e estruturação de uma aplicação web em React, Não
+        deve ser utilizado com aplicações maiores pois não conta com a estrutura
+        completa de uma aplicação.
+      </h2>
+    </div>
+  );
+};
+
+export default App;
